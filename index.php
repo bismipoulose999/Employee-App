e81c0491aec6f43a716b5a3d01b1dcd1bdce5536
bismipoulose999 <bismipoulose999@gmail.com>
2020-12-03 17:06:53 +0530

    css added

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body>
-    <h3>Employee</h3>
+<body class="bgcolor">
+    
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h3>Employee</h3>
     <table class="table">
     <tr>
         <td>Employee Name</td>
@@ -46,5 +51,8 @@
         <td><button class="btn btn-success">Register</button></td>
     </tr>
     </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
